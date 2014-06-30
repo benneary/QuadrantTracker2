893f7158b1c4934bf0bae0966d22455f92b8e9eb
benjaminneary <BenjaminNeary@aol.com>
2014-06-30 14:50:40 -0500

    lots of stuff has happened
    yeah yeah yeah

--- Scripts/quadrants.js
+++ Scripts/quadrants.js
@@ -1,16 +1,27 @@
-$( "#side-nav-invisible" ).hover(
+$( "#side-nav-invisible" ).mouseenter(
 	function(){
 		$("#side-nav")
 		.velocity("stop")
 		.velocity("transition.slideLeftIn", { duration: 1000 })
-	},
+	});
+
+$( "#side-nav-invisible-exit" ).mouseleave(
 	function(){
+		console.log("the mouse has left the building")
 		$("#side-nav")
 		.velocity("stop")
 		.velocity("transition.slideLeftOut", { duration: 1000 })
-	}
-	);
+	});
 
+$( "#filter-btn" ).click(function() {
+	$("#filter-modal")
+	.velocity("fadeIn", {duration: 800 })
+});
+
+$( "#filter-close-btn" ).click(function() {
+	$("#filter-modal")
+	.velocity("fadeOut", {duration: 800 })
+});
 
 
 $( ".task" ).hover(
@@ -28,13 +39,13 @@ $( ".task" ).hover(
 	);
 
 var nav_visible = false;
-$(".menu-btn").tap(function(){
-	if (nav_visible){
-		nav_visible = true;
-		$("#side-nav-mobile").velocity("transition.slideLeftIn", { duration: 1000 })
-	} 
-	else {
-		nav_visible = false;
-		$("side-nav-mobile").velocity("transition.slideLeftOut", { duration: 1000 })
-}
-});
+// $(".menu-btn").tap(function(){
+// 	if (nav_visible){
+// 		nav_visible = true;
+// 		$("#side-nav-mobile").velocity("transition.slideLeftIn", { duration: 1000 })
+// 	} 
+// 	else {
+// 		nav_visible = false;
+// 		$("side-nav-mobile").velocity("transition.slideLeftOut", { duration: 1000 })
+// }
+// });
